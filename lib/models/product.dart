57b39f95499496d3c8e0e711a9f9d8d1837888c9
CdemyLeitung <leitung@cdemy.de>
2025-06-13 12:26:27 +0200

    Refactor Product class documentation for clarity

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,9 +1,11 @@
+/// Represents a single product in the machine.
 class Product {
   final String name;
-  final String url;
-  final int price;
+  final String url; // Internet url of image (including extension).
+  final int price; // Price in Euro cents.
   final int amount;
 
+  /// Constructs a single product in the machine.
   Product({
     required this.name,
     required this.url,
