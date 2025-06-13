caba0297d3091b933a271bb75afcd3b30417d79c
CdemyLeitung <leitung@cdemy.de>
2025-06-13 12:05:33 +0200

    Add Product model class and update analysis options for formatter

---
+++ lib/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String name;
+  final String url;
+  final int price;
+  final int amount;
+
+  Product({
+    required this.name,
+    required this.url,
+    required this.price,
+    required this.amount,
+  });
+}
