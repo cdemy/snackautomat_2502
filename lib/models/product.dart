c6563f865f2116caa6bc93d010915ed0aa112926
CdemyLeitung <leitung@cdemy.de>
2025-06-16 15:24:28 +0200

    refactor: reorder and clarify comments in Product model

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,8 +1,15 @@
 /// Represents a single product in the machine.
 class Product {
+  /// The name of the product.
   final String name;
-  final String url; // Internet url of image (including extension).
-  final int price; // Price in Euro cents.
+
+  /// The URL of the product image.
+  final String url;
+
+  /// The price of the product in Euro cents.
+  final int price;
+
+  /// The amount of this product in the machine.
   final int amount;
 
   /// Constructs a single product in the machine.
